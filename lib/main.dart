be5c4e74258f419cfc6886008d701ddb9e06ac33
codeislife07 <ravisondarva7284@gmail.com>
2025-02-02 10:49:43 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:upi_payment_plugin/model/upi_app_model.dart';
+import 'package:upi_payment_plugin/upi_payment_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<UpiAppModel> upiApps = [];
+  UpiAppModel? selectedUpiApp;
+
+  @override
+  void initState() {
+    fetchUpiApps();
+    super.initState();
+  }
+
+  Future<void> fetchUpiApps() async {
+    List<UpiAppModel> apps = await UpiPaymentPlugin().getUpiApps();
+    setState(() {
+      upiApps = apps;
+    });
+  }
+
+  void initiateUPIPayment() {
+    if (selectedUpiApp == null) return;
+    UpiPaymentPlugin.initiateUPIPayment(
+      payeeUpiId: 'upi@id',
+      payeeName: 'payeeName',
+      amount: 1.0,
+      transactionId: 'txn123456',
+      transactionNote: 'Test Transaction',
+      merchantCode: '1234',
+      link: '',
+      transactionRefId: 'ref123456',
+      packageName:selectedUpiApp?.packageName?? 'com.google.android.apps.nbu.paisa.user',//default google pay
+      secretKey: '', //provide by upi app
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("UPI Payment")),
+        body: Center(
+          child: Column(
+            children: [
+              DropdownButton<UpiAppModel>(
+                hint: Text("Select UPI App"),
+                value: selectedUpiApp,
+                onChanged: (UpiAppModel? newValue) {
+                  setState(() {
+                    selectedUpiApp = newValue;
+                  });
+                },
+                items: upiApps.map((UpiAppModel app) {
+                  return DropdownMenuItem<UpiAppModel>(
+                    value: app,
+                    child: Text(app.appName),
+                  );
+                }).toList(),
+              ),
+              ElevatedButton(
+                onPressed: initiateUPIPayment,
+                child: Text("Pay Now"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
